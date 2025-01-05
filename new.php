4eb8b21aaf211d85a41f3918bca086298dc42846
realJOSIBU <sibu_6@gmx.ch>
2025-01-05 16:46:23 +0100

    Create new.php

---
+++ new.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "hi"
+/*
+system("dir")
+
+$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("webdictionary.txt"));
+fclose($myfile);
+*/
+?>
